4faa4d8a86cb4214d7ba688c651db093e10781a5
Celio <buericelio@cy-tech.fr>
2023-11-29 14:57:27 +0100

    update producer and gps tracker

--- producer/app/main.py
+++ producer/app/main.py
@@ -2,30 +2,31 @@ import asyncio
 import os
 
 from fastapi import FastAPI
-# from ProducerInterface import ProducerInterface
+from ProducerInterface import ProducerInterface
 from GPSCoordinates import GPSTracker
 import logging
 logging.basicConfig(level=logging.INFO)
 
 
 conf = {
-    'kefka_host': os.environ.get('KAFKA_HOST'),
+    'kafka_host': os.environ.get('KAFKA_HOST'),
     'client_id': os.environ.get('CLIENT_ID'),
     'topic': os.environ.get('TOPIC'),
 }
+
 print("Launching app")
 app = FastAPI()
 
 @app.on_event('startup')
 async def main_loop():
-    print('Starting up producer')
-    # producer = ProducerInterface(conf)
-    print('Starting up GPS tracker')
+    logging.info('Starting up producer')
+    producer = ProducerInterface(conf)
+    logging.info('Starting up GPS tracker')
     gps_tracker = GPSTracker()
-    while True:
+    if True:
         gps_tracker.update()
-        # producer.produce(
-        #     gps_tracker.get_gps_coordinates()
-        # )
-        logging.info(f"Je suis à : {gps_tracker.get_gps_coordinates()}")
+        producer.produce(
+            gps_tracker.get_gps_coordinates()
+        )
+        logging.info(f"Sending coordinates: {gps_tracker.get_gps_coordinates()})
         await asyncio.sleep(1)
