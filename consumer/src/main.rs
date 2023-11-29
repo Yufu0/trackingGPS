07256947ca677fde61abf44120d86d827d285ede
Lantaxel <lantaaxel@gmail.com>
2023-11-29 14:59:49 +0100

    feat & fix: first kafka and update init

--- consumer/src/main.rs
+++ consumer/src/main.rs
@@ -1,52 +1,61 @@
-use std::time::SystemTime;
+use std::time::{Duration, UNIX_EPOCH};
+use serde::Deserialize;
 use tokio_postgres::{NoTls, Error};
+use kafka::consumer::{Consumer, FetchOffset, GroupOffsetStorage};
 
-#[derive(Debug)]
+#[derive(Deserialize,Debug)]
 struct Coordonnees {
-    id: i32,
+    id: String,
     longitude: f64,
     latitude: f64,
-    date: SystemTime,
+    date: u64,
 }
 
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
+async fn main(){
 
     let db_url = "postgresql://postgres:postgres@localhost/tracker";
 
     let (client, connection) = tokio_postgres::connect(db_url, NoTls).await?;
-    println!("a");
+
     // Spawn a task to handle the connection
     tokio::spawn(async move {
         if let Err(e) = connection.await {
             eprintln!("connection error: {}", e);
         }
     });
-    println!("a");
 
-
-    let co = Coordonnees {
-        id: 1,
-        longitude: -63572375290155.0,
-        latitude: 106744840359415.0,
-        date: SystemTime::now(),
-    };
-    println!("a");
-
-    put_item(&client, &co).await?;
-    println!("a");
-
-    Ok(())
+    let mut consumer =
+        Consumer::from_hosts(vec!("localhost:9092".to_owned()))
+            .with_topic_partitions("coordinates".to_owned(), &[0, 1])
+            .with_fallback_offset(FetchOffset::Earliest)
+            .with_group("my-group".to_owned())
+            .with_offset_storage(Some(GroupOffsetStorage::Kafka))
+            .create()
+            .unwrap();
+    loop {
+        for ms in consumer.poll().unwrap().iter() {
+            for m in ms.messages() {
+                println!("{:?}", m);
+                /*
+                    let res : Coordonnees = serde_json::from_str(m).unwrap();
+                    put_item(&client, res).await.expect("Error inserting into PostgreSQL");
+                */
+            }
+            consumer.consume_messageset(ms).expect("TODO: panic message");
+        }
+        consumer.commit_consumed().unwrap();
+    }
 }
 
 
 
-async fn put_item(client: &tokio_postgres::Client, co: &Coordonnees) -> Result<(), Error> {
+async fn put_item(client: &tokio_postgres::Client, co: Coordonnees) -> Result<(), Error> {
     client
         .execute(
             "INSERT INTO item_tracker (id, longitude, latitude, datetime) VALUES ($1, $2, $3, $4)",
-            &[&co.id, &co.longitude, &co.latitude, &co.date],
+            &[&co.id, &co.longitude, &co.latitude, &(UNIX_EPOCH + Duration::from_secs(co.date))],
         )
         .await?;
 
--- init.sql
+++ init.sql
@@ -3,9 +3,9 @@ CREATE DATABASE IF NOT EXISTS tracker;
 \c tracker;
 
 CREATE TABLE IF NOT EXISTS item_tracker (
-    id INT,
-    datetime TIMESTAMP,
-    longitude FLOAT,
-    latitude FLOAT,
+    id CHAR(5) NOT NULL,
+    datetime TIMESTAMP NOT NULL,
+    longitude FLOAT NOT NULL,
+    latitude FLOAT NOT NULL,
     PRIMARY KEY (id, datetime)
 );
