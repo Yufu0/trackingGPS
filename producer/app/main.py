5de167ae9338db3468eb3563e0a4d373279f4863
Celio <buericelio@cy-tech.fr>
2023-11-29 16:28:43 +0100

    update docker compose kafka

--- producer/app/main.py
+++ producer/app/main.py
@@ -15,7 +15,7 @@ logging.basicConfig(level=logging.INFO)
 # }
 
 conf = {
-    'kafka_host': '172.19.0.3:9092',
+    'kafka_host': '0.0.0.0:9092',
     'client_id': '1',
     'topic': 'coordinates',
 }
@@ -30,48 +30,17 @@ async def main_loop():
     producer = ProducerInterface(conf)
     logging.info('Starting up GPS tracker')
     gps_tracker = GPSTracker()
-    if True:
+    while True:
         gps_tracker.update()
         producer.produce(
-            gps_tracker.get_gps_coordinates()
+            {
+                "id": conf['client_id'],
+                "longitude": gps_tracker.get_gps_coordinates().longitude,
+                "latitude": gps_tracker.get_gps_coordinates().latitude,
+                "date": str(gps_tracker.datetime),
+            }
         )
         logging.info(f"SEND")
         # logging.info(f"Je suis à : {gps_tracker.get_gps_coordinates()}")
         await asyncio.sleep(1)
 
-
-
-
-from confluent_kafka import Consumer
-@app.on_event('startup')
-async def main_loop2():
-    c = Consumer({
-        'bootstrap.servers': '172.19.0.3:9092',
-        'group.id': '1',
-        'auto.offset.reset': 'earliest'
-    })
-
-    c.subscribe(['coordinates'])
-    producer = ProducerInterface(conf)
-
-    from time import time
-
-    while True:
-        a = time()
-        producer.produce(
-            {"a": str(a)}
-        )
-        logging.info(f"SEND")
-        await asyncio.sleep(1)
-        msg = c.poll(1.0)
-
-        if msg is None:
-            logging.info("No message received by consumer")
-            continue
-        if msg.error():
-            logging.info("Consumer error: {}".format(msg.error()))
-            continue
-
-        logging.info(f'Received message: {msg.value()} {time()}')
-
-    c.close()
